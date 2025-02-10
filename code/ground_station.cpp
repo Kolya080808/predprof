90be18cbf22aa8ffa440071a3b1775dfa3d3aa8f
Nikolay <81642661+Kolya080808@users.noreply.github.com>
2025-02-10 20:00:35 +0300

    Update ground_station.cpp

--- code/ground_station.cpp
+++ code/ground_station.cpp
@@ -1,5 +1,5 @@
-#include <StandardCplusplus.h> 
-#include <SPI.h> 
+#include <StandardCplusplus.h> // Для работы библиотек iostream, vector, string, sstream
+#include <SPI.h>  
 #include "nRF24L01.h" 
 #include "RF24.h" 
 #include <Wire.h> 
@@ -10,10 +10,15 @@
 #include <sstream> 
 #include <LiquidCrystal_I2C.h> 
 LiquidCrystal_I2C lcd(0x27, 20, 2); 
+long timer;
+long wait;
+long wait2;
+long timer2;
 Servo servo; 
- 
-using namespace std; 
- 
+String messange1="";
+String messange2="";
+String fullmessange = "";
+using namespace std;
 String decryptionMessage(const vector<int> &encrypted, int a, int b) { 
  String decrypted; 
  int a_inv = 0; 
@@ -32,157 +37,92 @@ String decryptionMessage(const vector<int> &encrypted, int a, int b) {
  
  return decrypted; 
 } 
- 
- 
- 
-RF24 radio(9, 10); 
-byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; 
-void setup() { 
+String encryptedMessage; 
+int a = 3; 
+int b = 5; 
+RF24 radio(9, 10);
+byte address[][6] =  {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"};
+void setup() {
  Serial.begin(9600); 
  radio.begin(); 
  radio.setAutoAck(1); 
  radio.setRetries(0, 15); 
  radio.enableAckPayload(); 
- radio.setPayloadSize(32); 
+ radio.setPayloadSize(64); 
  radio.openReadingPipe(1, address[0]); 
  radio.setChannel(0x60); 
  radio.setPALevel(RF24_PA_MAX); 
  radio.setDataRate(RF24_250KBPS); 
  radio.powerUp(); 
  radio.startListening(); 
- Serial.println("Радио готово к приему сообщений..."); 
- lcd.init(); 
- servo.attach(2); 
- servo.write(127); 
-} 
- 
- 
-String encryptedMessage; 
-int a = 3; 
-int b = 5; 
- 
- 
-void loop() { 
+ Serial.println("Радио готово к приему сообщений...");
+ lcd.init();
+ lcd.backlight();
+ timer=millis();
+ wait=millis();
+ timer2 = millis();
+ wait2 = millis();
+}
+void loop() {
+  byte pipino;
+  byte read;
+  int print= 1;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  if(timer2-wait2>=10000){
+    float temp = analogRead(A0);
+    lcd.print("temp ");
+    lcd.print(temp);
+    lcd.print("C");
+    timer2 = millis();
+    wait2 = millis();
+    lcd.clear();
+    lcd.setCursor(0,0);
+  }
+  // radio.read(&read,1);
+  // if (read = 1){
+  //   radio.openWritingPipe( address[0]);
+  //   radio.stopListening();
+  //   radio.write(&print,1);
+  //   radio.openReadingPipe(1, address[0]);
+  //   radio.startListening();
+  // }
+  String encryptedMessage;
+  char messange[100];
+  while(radio.available(&pipino)){
+    radio.read(&messange,sizeof(messange));
+    for(byte i = 0;i<sizeof(messange);i++){
+      encryptedMessage += messange[i];
+    }
+    vector<int> encryptedForDecryption; 
+    stringstream ss(encryptedMessage.c_str()); 
+    int num; 
+    while (ss >> num) { 
+    encryptedForDecryption.push_back(num);  decryptionMessage(encryptedForDecryption, a, b); 
+    }
+    if((timer-wait)<=1999 and decryptionMessage(encryptedForDecryption, a, b) != messange2) {
+       messange1 = decryptionMessage(encryptedForDecryption, a, b); 
+    }
+    if ((timer-wait)>=1015 and decryptionMessage(encryptedForDecryption, a, b) !=messange1) {
+      messange2 = decryptionMessage(encryptedForDecryption, a, b);
+      timer = millis();
+      wait = millis();
+        fullmessange =  messange1 + messange2;
+        Serial.println("messange:" + fullmessange);
+        messange1="";
+        messange2="";
+      }
+    }
+  else{
+    lcd.clear();
+    lcd.setCursor(0,0)
+    lcd.print(fullmessange);
+  } 
+  // первое сообщение
 
-
- String decryptedAzimyth; 
- radio.setChannel(0x60); 
- lcd.clear(); 
- lcd.setCursor(0,0); 
- float temp = analogRead(A0); 
- lcd.print("temp "); 
- lcd.print(temp); 
- lcd.print("C"); 
- if(radio.getChannel() == 0x60) { 
-  char receivedData[32]; 
-  byte pipeNo; 
-  while (radio.available(&pipeNo)) { 
-   radio.read(&receivedData, sizeof(receivedData)); 
-  } 
-  for (byte i = 0; i < sizeof(receivedData); i++) { 
-   encryptedMessage += receivedData[i]; 
-  } 
-  Serial.println(encryptedMessage); 
-  memset(receivedData, 0, sizeof(receivedData));
- 
- 
-  vector<int> encryptedForDecryption; 
-  stringstream ss(encryptedMessage.c_str()); 
-  int num; 
-  while (ss >> num) { 
-   encryptedForDecryption.push_back(num); 
-  } 
- 
-  decryptedAzimyth = decryptionMessage(encryptedForDecryption, a, b); 
- 
-  Serial.println(decryptedAzimyth); 
- 
- 
-  radio.setChannel(0x61); 
- 
- } 
- 
- 
- if(radio.getChannel() == 0x61) { 
-  char receivedData[32]; 
-  byte pipeNo; 
-  while (radio.available(&pipeNo)) { 
-   radio.read(&receivedData, sizeof(receivedData)); 
-  } 
-  for (byte i = 0; i < sizeof(receivedData); i++) { 
-   encryptedMessage += receivedData[i]; 
-  } 
-  Serial.println(encryptedMessage); 
-  memset(receivedData, 0, sizeof(receivedData));
- 
- 
-  vector<int> encryptedForDecryption; 
-  stringstream ss(encryptedMessage.c_str()); 
-  int num; 
-  while (ss >> num) { 
-   encryptedForDecryption.push_back(num); 
-  } 
- 
-  String decryptedAzimyth = decryptionMessage(encryptedForDecryption, a, b); 
- 
-  Serial.println(decryptedAzimyth); 
- 
- 
- 
- 
- 
-  while (radio.available(&pipeNo)) { 
-   radio.read(&receivedData, sizeof(receivedData)); 
-  } 
-  for (byte i = 0; i < sizeof(receivedData); i++) { 
-   encryptedMessage += receivedData[i]; 
-  } 
-  Serial.println(encryptedMessage); 
-  memset(receivedData, 0, sizeof(receivedData));
- 
- 
-  while (ss >> num) { 
-   encryptedForDecryption.push_back(num); 
-  } 
- 
-  decryptedAzimyth = decryptionMessage(encryptedForDecryption, a, b); 
- 
-  Serial.println(decryptedAzimyth); 
- 
- 
- 
- 
- 
- 
-  while (radio.available(&pipeNo)) { 
-   radio.read(&receivedData, sizeof(receivedData)); 
-  } 
-  for (byte i = 0; i < sizeof(receivedData); i++) { 
-   encryptedMessage += receivedData[i]; 
-  } 
-  Serial.println(encryptedMessage); 
-  memset(receivedData, 0, sizeof(receivedData));
- 
- 
-  while (ss >> num) { 
-   encryptedForDecryption.push_back(num); 
-  } 
- 
-  decryptedAzimyth = decryptionMessage(encryptedForDecryption, a, b);
-  Serial.println(decryptedAzimyth);
-  radio.setChannel(0x60);
- }
- lcd.clear();
- lcd.setCursor(0,0);
- lcd.print("Azimyth ");
- lcd.print(decryptedAzimyth);
- lcd.print(" ");
- lcd.print(decryptedAzimyth);
- lcd.setCursor(0,1);
- lcd.print(decryptedAzimyth);
- lcd.print(" ");
- lcd.print(decryptedAzimyth);
- delay(10000);
-
-
+  // lcd.print(newmassage);
+  memset(messange,0,sizeof(messange));
+  delay(1000);
+  wait = (wait-1000);
+  wait2 = (wait2-1000);
 }
